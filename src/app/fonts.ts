c804ec4e999aa4688c5fec9a5e086761dc538860
Nii Dromo <michaelperryt97@gmail.com>
2026-08-01 12:13:56 +0000

    created new font schemes

---
+++ src/app/fonts.ts
@@ -0,0 +1,19 @@
+import { Manrope, Space_Grotesk, Caveat } from "next/font/google";
+
+export const manrope = Manrope({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-manrope",
+});
+
+export const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-space",
+});
+
+export const caveat = Caveat({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-caveat",
+});
